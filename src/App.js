1947f3a7ea37db0f7f6b3f7a26bd8492ebb4c65f
amoljore7 <amoljore7@gmail.com>
2020-05-20 12:04:40 +0530

    Login with google auth completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
+import GoogleLogin from "react-google-login";
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  const responseGoogle = (response) => {
+    setName(response.profileObj.name);
+    setEmail(response.profileObj.email);
+    setImageUrl(response.profileObj.imageUrl);
+  }
   return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
+    <React.Fragment>
+      <h1>Login with Google</h1>
+      <h2>Welcome : {name}</h2>
+      <h2>email : {email}</h2>
+      {/* <img src={imageUrl} alt="email-image" /> */}
+      <GoogleLogin
+        clientId="1003906124625-2trrj2elg5sgoadssuom3fj99jknmrdj.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+    </React.Fragment>
   );
 }
 
